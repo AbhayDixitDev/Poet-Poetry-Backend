6ade97100008346d30fe2703491248c0dc470744
Abhay Dixit <dixitshivam534@gmail.com>
2025-01-27 01:17:23 +0530

    visitor can give review and see reviws and poet can also see reviews

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors({
     
     origin: "https://poetpoetry.netlify.app",
+    // origin: "http://localhost:5173",
     credentials: true
 }))
 
