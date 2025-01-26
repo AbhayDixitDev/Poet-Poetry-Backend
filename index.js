a331f38bff261c20b38ea820b3692ae90269590e
Abhay Dixit <dixitshivam534@gmail.com>
2025-01-27 01:29:33 +0530

    cors setting changed

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 
 
 app.use(cors({
-    origin: "*",
+    origin: ["https://poetpoetry.netlify.app", "http://localhost:5173"],
     credentials: true
 }))
 
